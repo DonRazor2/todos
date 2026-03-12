908675117c88809e12f9e1c992fa1676d7511a45
Florin <florinvisla@gmail.com>
2026-03-12 10:17:14 +0200

    boilerplate

---
+++ src/test/java/com/ketchup/todos/TodosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ketchup.todos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
