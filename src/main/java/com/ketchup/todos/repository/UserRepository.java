216e1268eee2f1c03a25704e03d8dc1b099b6513
Florin <florinvisla@gmail.com>
2026-03-13 10:41:52 +0200

    feat: added security (via spring security) with tokens and sessions

---
+++ src/main/java/com/ketchup/todos/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.ketchup.todos.repository;
+
+import com.ketchup.todos.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
